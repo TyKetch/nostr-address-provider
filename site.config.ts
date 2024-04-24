ff42d4d465f24f03e00f65db3a1bce0475245c59
Experilux_Dev <123844135+TyKetch@users.noreply.github.com>
2024-04-23 23:09:40 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,6 +1,6 @@
 export default {
   // your domain name
-  host: "zaps.lol",
+  host: "aapl.pro",
 
   // set to true to disable registration site-wide
   registrationDisabled: false,
@@ -10,7 +10,7 @@ export default {
 
   // your donation LNURL
   donationLNUrl:
-    "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AKXUATJD3CZ7JMPVDV9YCC4FVREX",
+    "lnbc1pnz3gw6pp5re6jkjzzhal7h5470edvlw05zwhrtkarqu5tk82v7lzkcfy9xnssdqqcqzzsxqyz5vqsp5u2y60w92vuh9rte2a7aq47llv3mu7m7gdkzyrz2dtmmxcgymqn9q9qyyssqjwk88wnr0ta4utc70lfmpkyc3m7w84c2hnjalfwwctgly9tquyrqulzy00mrtskm9nem2sl2c33ac0jzardm5mj64wc708zffd9qlqgpak5mes",
 
   // name of your MongoDB database
   dbName: "users",
